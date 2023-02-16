4ac2899bdcdab1b386f60da99cfd703f0a230c78
Samuel Neff <sneff@wayfair.com>
2023-02-15 20:18:58 -0500

    Basic test cases written

--- lib/src/main/kotlin/com/serndesign/stringcaser/CharSequenceExtensions.kt
+++ lib/src/main/kotlin/com/serndesign/stringcaser/CharSequenceExtensions.kt
@@ -1,3 +1,46 @@
 @file:JvmName("StringCaser")
 
 package com.serndesign.stringcaser
+
+private fun CharSequence.changeCase(): CharSequence {
+    return this
+}
+
+fun CharSequence.toCamelCase() =
+  changeCase()
+
+fun CharSequence.toFirstUpperCase() =
+  changeCase()
+
+fun CharSequence.toFirstLowerCase() =
+  changeCase()
+
+fun CharSequence.toKebabCase() =
+  changeCase()
+
+fun CharSequence.toPascalCase() =
+  changeCase()
+
+fun CharSequence.toScreamingKebabCase() =
+  changeCase()
+
+fun CharSequence.toScreamingSnakeCase() =
+  changeCase()
+
+fun CharSequence.toSentenceCase() =
+  changeCase()
+
+fun CharSequence.toSlugCase() =
+  changeCase()
+
+fun CharSequence.toSnakeCase() =
+  changeCase()
+
+fun CharSequence.toTitleCase() =
+  changeCase()
+
+fun CharSequence.toTrainCase() =
+  changeCase()
+
+fun CharSequence.toUpperCamelCase() =
+  changeCase()
